ef04c29b57b4572a61c0b0d90af0aaacfa0a6fc7
Dengjianping <djptux@gmail.com>
2021-08-23 14:33:17 +0800

    Add source code, and update readme

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# measure-block-time
+# A tool for measuring block time
+
+## Build
+```shell
+git clone 
+cd measure-block-time
+yarn
+```
+
+## Configure
+Change this line of code, point to the running node.
+```
+const nodeAddress = 'ws://127.0.0.1:9988';
+```
+
+## Run
+```shell
+node index.js
+```
+It should print log like this:
+```
+# 350 -> # 351 , block time:  11999
+average block time:  11999
+# 351 -> # 352 , block time:  12000
+average block time:  11999.5
+# 352 -> # 353 , block time:  11997
+average block time:  11998.666666666666
+# 353 -> # 354 , block time:  12008
+average block time:  12001
+# 354 -> # 355 , block time:  11991
+average block time:  11999
+# 355 -> # 356 , block time:  12004
+average block time:  11999.833333333334
+# 356 -> # 357 , block time:  11999
+average block time:  11999.714285714286
+```
---
+++ index.js
@@ -0,0 +1,31 @@
+const { ApiPromise, WsProvider } = require('@polkadot/api');
+
+// Create a promise API instance of the passed in node address.
+async function createPromiseApi(nodeAddress) {
+    const wsProvider = new WsProvider(nodeAddress);
+    const api = await new ApiPromise({ provider: wsProvider });
+    await api.isReady;
+    return api;
+}
+
+async function main() {
+    // const nodeAddress = 'wss://rpc.polkadot.io';
+    const nodeAddress = 'ws://127.0.0.1:9988';
+    const api = await createPromiseApi(nodeAddress);
+
+    let count = 0;
+    let all_block_time = 0;
+    const unsub = await api.rpc.chain.subscribeNewHeads(async (lastHeader) => {  
+        const lastBlockNum = lastHeader.number.toNumber();
+        const momentCurrent = await api.query.timestamp.now.at(lastHeader.hash);
+        const momentPrev = await api.query.timestamp.now.at(lastHeader.parentHash);
+        const blockTime = momentCurrent - momentPrev;
+        console.log("#", lastBlockNum - 1, "-> #",lastBlockNum, ", block time: ", blockTime);
+        
+        all_block_time += blockTime;
+        count += 1;
+        console.log("average block time: ", all_block_time / count);
+    });
+}
+
+main().catch(console.error);
