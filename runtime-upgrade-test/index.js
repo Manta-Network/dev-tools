c93bdfa13cd93a069bec9663884e83d7e8919aa6
ghzlatarev <ghzlatarev@gmail.com>
2021-08-29 01:53:25 +0300

    Use the file

--- runtime-upgrade-test/index.js
+++ runtime-upgrade-test/index.js
@@ -23,7 +23,7 @@ async function main() {
     const PHRASE = fs.readFileSync('root_mnemonics').toString();
     console.log(PHRASE);
     // Add an account, straight mnemonic
-    const newPair = keyring.addFromUri('//Alice', { name: 'Alice default' });
+    const newPair = keyring.addFromUri(PHRASE, { name: 'Alice default' });
 
     // Retrieve the runtime to upgrade
     const code = fs.readFileSync('calamari.wasm').toString('hex');
