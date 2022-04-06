7fd2d7813d2caa9bdb243c06506ccb1d6810eadb
ghzlatarev <ghzlatarev@gmail.com>
2022-04-06 17:51:25 +0300

    Add tool for caclulating parachains' sovereign accounts

---
+++ caclulate-sovereign-account/README.md
@@ -0,0 +1,19 @@
+# A tool to calculate your parachain's sovereign account on Rococo.
+
+## Build
+```shell
+git clone git@github.com:Manta-Network/Dev-Tools.git
+cd Dev-Tools/calculate-sovereign-account
+yarn
+```
+
+## Run
+```shell
+ts-node calculateSovereignAddress --paraid ID
+```
+It should print log like this:
+```
+Sovereign Account Address on Relay: 0x7061726124080000000000000000000000000000000000000000000000000000
+Sovereign Account Address on other Parachains (Generic): 0x7369626c24080000000000000000000000000000000000000000000000000000
+Sovereign Account Address on Moonbase Alpha: 0x7369626c24080000000000000000000000000000
+```
---
+++ caclulate-sovereign-account/calculateSovereignAddress.ts
@@ -0,0 +1,37 @@
+import { u8aToHex } from '@polkadot/util';
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import { ParaId } from '@polkadot/types/interfaces';
+import yargs from 'yargs';
+
+const args = yargs.options({
+  'para-id': { type: 'string', demandOption: true, alias: 'paraid' },
+}).argv;
+
+const main = async () => {
+  const relayProvider = new WsProvider(
+    'wss://rococo-rpc.polkadot.io'
+  );
+
+  const relayApi = await ApiPromise.create({
+    provider: relayProvider,
+  });
+
+  const targetParaId: ParaId = relayApi.createType('ParaId', args['para-id']);
+
+  const sovAddressRelay = u8aToHex(
+    new Uint8Array([...new TextEncoder().encode('para'), ...targetParaId.toU8a()])
+  ).padEnd(66, '0');
+
+  const sovAddressPara = u8aToHex(
+    new Uint8Array([...new TextEncoder().encode('sibl'), ...targetParaId.toU8a()])
+  ).padEnd(66, '0');
+
+  console.log(`Sovereign Account Address on Relay: ${sovAddressRelay}`);
+  console.log(`Sovereign Account Address on other Parachains (Generic): ${sovAddressPara}`);
+  console.log(`Sovereign Account Address on Moonbase Alpha: ${sovAddressPara.slice(0, 42)}\n\n`);
+
+  await relayApi.disconnect();
+};
+
+main();
+
